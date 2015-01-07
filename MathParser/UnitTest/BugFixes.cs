678311ed86f9fbd7fedc4d88ef87859af4ca3978
artemlos <artem@artemlos.net>
2015-01-07 22:26:38 +0100

    some testing with doubles vs. decimals

--- MathParser/UnitTest/BugFixes.cs
+++ MathParser/UnitTest/BugFixes.cs
@@ -12,5 +12,21 @@ namespace UnitTest
             var mp = new Mathos.Parser.MathParser();
             var result = mp.Parse("-sin(5)");
         }
+
+        [TestMethod]
+        public void Doubles()
+        {
+
+            // the reason why decimal was chosen instead is because doubles are expressed as:
+            // 3.11111111111111E+29
+            // considering this, it should be possible to work with doubles instead.
+
+            double a = 311111111111111111111112222222d;
+
+            string b = a.ToString();
+
+            double c = double.Parse(b);
+
+        }
     }
 }
