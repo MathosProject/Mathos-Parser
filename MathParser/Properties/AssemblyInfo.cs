df0579e65a21c51c45064dd489ece65504e9893e
Ethan Dagner <napen123@gmail.com>
2017-07-26 16:52:42 -0600

    Remove unused references

--- MathParser/Properties/AssemblyInfo.cs
+++ MathParser/Properties/AssemblyInfo.cs
@@ -1,9 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-// Управление общими сведениями о сборке осуществляется с помощью 
-// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
-// связанные со сборкой.
 [assembly: AssemblyTitle("MathParser")]
 [assembly: AssemblyDescription("http://mathosproject.com/")]
 [assembly: AssemblyConfiguration("")]
@@ -15,15 +12,6 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 
-// Сведения о версии сборки состоят из следующих четырех значений:
-//
-//      Основной номер версии
-//      Дополнительный номер версии 
-//      Номер построения
-//      Редакция
-//
-// Можно задать все значения или принять номер построения и номер редакции по умолчанию, 
-// используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.1.0.0")]
 [assembly: AssemblyFileVersion("1.1.0.0")]
