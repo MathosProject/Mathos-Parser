55c610ebf4f8b55a3296933ed61a6f071501028f
dennisvg111 <dennisvg111@gmail.com>
2019-06-11 14:34:47 +0200

    Add a simple script parser, which can handle if statements
    Add a script parser which loops through lines given and executes them based on the state of a stack-based state machine.

---
+++ MathParser/ScriptParserException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Mathos.Parser
+{
+    public sealed class ScriptParserException : Exception
+    {
+        public ScriptParserException() : base()
+        {
+        }
+
+        public ScriptParserException(string message) : base(message)
+        {
+        }
+
+        public ScriptParserException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        public ScriptParserException(int line, Exception innerException) : base(innerException.GetType().Name + " on line " + line + ", " + Environment.NewLine + innerException.Message, innerException)
+        {
+        }
+    }
+}
